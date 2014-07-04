ae977e4d63ff8de6bcb5085dc29520c3a54c1d69
Tanaka Akira <akr@fsij.org>
2014-07-04 12:50:02 +0900

    more renames.

--- sample/interlocking_single_switch.rb
+++ sample/interlocking_single_switch.rb
@@ -2,9 +2,9 @@ require 'railway'
 
 facilities = Railway::Facilities.new
 
-facilities.add_point "s1", "n1", ["n2", 5], ["n3", 5]
+facilities.add_point "p1", "n1", ["n2", 5], ["n3", 5]
 
-facilities.add_route "r1", nil, [["n1", "n3", "s1"]]
+facilities.add_route "r1", nil, [["n1", "n3", "p1"]]
 
 facilities.each_segment {|rail, segment|
   facilities.add_circuit "c#{rail}", segment
@@ -22,8 +22,8 @@ fse = FSEvent.new(t0)
 fse.register_device(Railway::Dumper.new)
 fse.register_device(Railway::Circuit.new("circuit", facilities))
 fse.register_device(Railway::Interlocking.new("interlocking1", facilities))
-fse.register_device(FSEvent::ValueIdDevice2.new("vs1", "interlocking1", "s1"))
-fse.register_device(Railway::Point.new("s1", 1, "vs1", "s1"))
+fse.register_device(FSEvent::ValueIdDevice2.new("vp1", "interlocking1", "p1"))
+fse.register_device(Railway::Point.new("p1", 1, "vp1", "p1"))
 fse.register_device(FSEvent::ValueIdDevice2.new("vr1", "interlocking1", "r1"))
 fse.register_device(Railway::FixedSignal.new("r1", "vr1", "r1"))
 fse.register_device(Railway::Panel.new("panel", facilities, panel_plan))
--- sample/timedcontrol.rb
+++ sample/timedcontrol.rb
@@ -5,7 +5,7 @@ facilities.add_track "t1", "n1", "n2", 10
 facilities.add_track "t2", "n2", "n3", 10
 facilities.add_track "t3", "n3", "n4", 10
 
-facilities.add_point "s1", "n4", ["n5", 5], ["n8", 5]
+facilities.add_point "p1", "n4", ["n5", 5], ["n8", 5]
 
 facilities.add_track "t5", "n5", "n6", 10
 facilities.add_track "t6", "n6", "n7", 10
@@ -15,8 +15,8 @@ facilities.add_track "t8", "n8", "n9", 10
 facilities.add_track "t9", "n9", "n10", 10
 facilities.add_track "t10", "n10", "n11", 10
 
-facilities.add_route "r1", nil, [["n1", "n2", "t1"], ["n2", "n3", "t2"], ["n3", "n4", "t3"], ["n4", "n5", "s1"], ["n5", "n6", "t5"], ["n6", "n7", "t6"], ["n7", "n8", "t7"]]
-facilities.add_route "r2", nil, [["n1", "n2", "t1"], ["n2", "n3", "t2"], ["n3", "n4", "t3"], ["n4", "n8", "s1"], ["n8", "n9", "t8"], ["n9", "n10", "t9"], ["n10", "n11", "t10"]]
+facilities.add_route "r1", nil, [["n1", "n2", "t1"], ["n2", "n3", "t2"], ["n3", "n4", "t3"], ["n4", "n5", "p1"], ["n5", "n6", "t5"], ["n6", "n7", "t6"], ["n7", "n8", "t7"]]
+facilities.add_route "r2", nil, [["n1", "n2", "t1"], ["n2", "n3", "t2"], ["n3", "n4", "t3"], ["n4", "n8", "p1"], ["n8", "n9", "t8"], ["n9", "n10", "t9"], ["n10", "n11", "t10"]]
 
 facilities.each_segment {|rail, segment|
   facilities.add_circuit "c#{rail}", segment
@@ -125,7 +125,7 @@ fse.register_device(Railway::Dumper.new)
 fse.register_device(train = Railway::Train.new("train1", 15, ["r2"], facilities))
 fse.register_device(Railway::Circuit.new("circuit", facilities))
 fse.register_device(ScheduledSignal.new("r2", [[t0, 0], [t0+7, 1], [t0+13,0]]))
-fse.register_device(ScheduledPoint.new("s1", [[t0, 1], [t0+6, 2], [t0+30,1]]))
+fse.register_device(ScheduledPoint.new("p1", [[t0, 1], [t0+6, 2], [t0+30,1]]))
 
 #pp facilities
 
