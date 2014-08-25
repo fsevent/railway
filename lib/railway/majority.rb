7d8a9186ff41ee535aacd1cdfe0496243f8b241b
Tanaka Akira <akr@fsij.org>
2014-08-25 17:20:11 +0900

    majority device implemented.

---
+++ lib/railway/majority.rb
@@ -0,0 +1,142 @@
+class Railway::Majority < FSEvent::AbstractDevice
+  def initialize(device_name, status_name, input_device_names, count_status_name=nil)
+    super device_name
+    @status_name = status_name
+    if input_device_names.length < 2
+      raise "Not enough input devices for #{device_name}"
+    end
+    @input_device_names_original = input_device_names
+    @input_device_names = input_device_names.dup
+    @minimum_num_devices = 2
+    @count_status_name = count_status_name
+    @count_mismatch_max = 2 # second
+    @soundness = nil
+  end
+
+  def registered
+    @output = nil
+    define_status(@status_name, @output)
+    @input_device_names.each {|input_device_name|
+      add_watch(input_device_name, @status_name)
+      if @count_status_name
+        add_watch(input_device_name, @count_status_name)
+      end
+    }
+    @count_mismatch_limit = @framework.current_time + @count_mismatch_max
+    define_status("soundness", @soundness)
+  end
+
+  def check_count(watched_status, changed_status)
+    if @input_device_names.any? {|input_device_name| !watched_status.has_key?(input_device_name) ||
+                                                          !watched_status[input_device_name].has_key?(@count_status_name) ||
+                                                          watched_status[input_device_name][@count_status_name] == nil }
+      return
+    end
+    if @input_device_names.map {|input_device_name| watched_status[input_device_name][@count_status_name] }.uniq.length == 1
+      @count_mismatch_limit = nil
+      return @input_device_names
+    end
+    if !@count_mismatch_limit
+      @count_mismatch_limit = @framework.current_time + @count_mismatch_max
+      @schedule.merge_schedule [@count_mismatch_limit]
+      return
+    elsif @framework.current_time < @count_mismatch_limit
+      return
+    end
+    count_hash = {}
+    @input_device_names.each {|input_device_name|
+      count = watched_status[input_device_name][@count_status_name]
+      count_hash[count] ||= []
+      count_hash[count] << input_device_name
+    }
+    count_list = count_hash.to_a.sort_by {|count, devices| devices.length }
+    if 2 < count_list.length
+      broken_devices = count_list[0...-2].map {|count, devices| devices }.flatten
+      @input_device_names -= broken_devices
+      count_list[0...-2] = []
+    end
+    if @input_devce_names.length < 2
+      soundness = false
+      if @soundness != soundness
+        modify_status("soundness", soundness)
+        @soundness = soundness
+      end
+      return
+    end
+    if count_list.length == 2
+      (count1, count1_devices), (count2, count2_devices) = count_list
+      if count1_devices.length == count2_devices.length
+        if count1 > count2
+          # prefer larger count.
+          count1, count1_devices, count2, count2_devices = count2, count2_devices, count1, count1_devices
+        end
+      end
+    end
+    (max_count, max_count_devices) = count_list.last
+    unless 2 <= max_count_devices.length
+      return
+    end
+    return max_count_devices
+  end
+
+  def check_value(input_device_names, watched_status, changed_status)
+    if @input_device_names.any? {|input_device_name| !watched_status.has_key?(input_device_name) ||
+                                                     !watched_status[input_device_name].has_key?(@status_name) ||
+                                                     watched_status[input_device_name][@status_name] == nil }
+      return
+    end
+    value_hash = {}
+    input_device_names.each {|input_device_name|
+      value = watched_status[input_device_name][@status_name][0]
+      value_hash[value] ||= []
+      value_hash[value] << input_device_name
+    }
+    value_list = value_hash.to_a.sort_by {|value, devices| devices.length }
+    if 2 < value_list.length
+      broken_devices = value_list[0...-2].map {|value, devices| devices }.flatten
+      @input_device_names -= broken_devices
+      value_list[0...-2] = []
+    end
+    if @input_device_names.length < 2
+      soundness = false
+      if @soundness != soundness
+        modify_status("soundness", soundness)
+        @soundness = soundness
+      end
+      return
+    end
+    if value_list.length == 2
+      (value1, value1_devices), (value2, value2_devices) = value_list
+      unless value1_devices.length < value2_devices.length
+        return
+      end
+      value_list.shift
+    end
+    (value1, value1_devices), = value_list
+    unless 2 <= value1_devices.length
+      return
+    end
+    return value1_devices
+  end
+
+  def run(watched_status, changed_status)
+    if @count_status_name
+      input_device_names = check_count(watched_status, changed_status)
+      if !input_device_names
+        return
+      end
+    else
+      input_device_names = @input_device_names
+    end
+    input_device_names = check_value(input_device_names, watched_status, changed_status)
+    if !input_device_names
+      return
+    end
+    output = watched_status[input_device_names.first][@status_name]
+    if output != @output
+      @output = output
+      modify_status(@status_name, @output)
+    end
+  end
+end
+
---
+++ sample/majority_passing_loop.rb
@@ -0,0 +1,100 @@
+require 'railway'
+
+facilities = Railway::Facilities.new
+
+# n1 --- n2 - p1 - n3 --- n4
+#              \           \
+#               n8 --- n7 - p2 - n6 --- n5
+#
+# r1: n1 - n2
+# r2: n2 -p1- n3 - n4
+# r4: n4 -p2- n7 - n8
+# r5: n5 - n6
+# r6: n6 -p2- n7 - n8
+# r8: n8 -p1- n2 - n1
+
+facilities.add_track "t1", "n1", "n2", 200
+facilities.add_point "p1", "n2", ["n3", 5], ["n8", 5]
+facilities.add_track "t3", "n3", "n4", 200
+facilities.add_track "t5", "n5", "n6", 200
+facilities.add_point "p2", "n6", ["n7", 5], ["n4", 5]
+facilities.add_track "t7", "n7", "n8", 200
+
+facilities.add_route "r1", nil, [["n1", "n2", "t1"]]
+facilities.add_route "r2", nil, [["n2", "n3", "p1"], ["n3", "n4", "t3"]]
+facilities.add_route "r4", nil, [["n4", "n6", "p2"], ["n6", "n5", "t5"]]
+
+facilities.add_route "r5", nil, [["n5", "n6", "t5"]]
+facilities.add_route "r6", nil, [["n6", "n7", "p2"], ["n7", "n8", "t7"]]
+facilities.add_route "r8", nil, [["n8", "n2", "p1"], ["n2", "n1", "t1"]]
+
+facilities.each_segment {|rail, segment|
+  facilities.add_circuit "c#{rail}", segment
+  facilities.add_area segment, rail
+}
+
+t0 = Time.utc(2000)
+
+panel_plan = [
+  [t0+10, "r1", true],
+  [t0+10, "r5", true],
+  [t0+20, "r2", true],
+  [t0+20, "r6", true],
+  [t0+15, "r1", false],
+  [t0+15, "r5", false],
+  [t0+30, "r2", false],
+  [t0+30, "r6", false],
+  [t0+35, "r4", true],
+  [t0+35, "r8", true],
+  [t0+45, "r4", false],
+  [t0+45, "r8", false],
+]
+
+fse = FSEvent.new(t0)
+#fse.register_device(FSEvent::DebugDumper.new)
+fse.register_device(Railway::Dumper.new)
+fse.register_device(Railway::Panel.new("panel", facilities, panel_plan))
+fse.register_device(Railway::Circuit.new("circuit", facilities))
+
+latch_inputs = []
+
+facilities.each_route_and_fixedsignal_name {|route, signal|
+  latch_inputs << ["panel", route]
+  latch_inputs << [signal, signal]
+}
+facilities.circuit.values.uniq.each {|circuit|
+  latch_inputs << ["circuit", circuit]
+}
+facilities.point.each_key {|point|
+  latch_inputs << [point, point]
+}
+
+interlocking_list = []
+%w[a b c].each {|i|
+  interlocking = "interlocking_#{i}"
+  fse.register_device(Railway::Interlocking.new(interlocking, "latch", "latch", facilities, "latch"))
+  interlocking_list << interlocking
+}
+
+facilities.point.each_key {|point|
+  fse.register_device(Railway::Majority.new("majority_#{point}", point, interlocking_list, "count"))
+  fse.register_device(FSEvent::ValueIdDevice2.new("v_#{point}", "majority_#{point}", point))
+  fse.register_device(Railway::Point.new(point, 1, "v_#{point}", point))
+}
+
+facilities.each_fixedsignal_name {|signal|
+  fse.register_device(Railway::Majority.new("majority_#{signal}", signal, interlocking_list, "count"))
+  fse.register_device(FSEvent::ValueIdDevice2.new("v_#{signal}", "majority_#{signal}", signal))
+  fse.register_device(Railway::FixedSignal.new(signal, "v_#{signal}", signal))
+}
+
+fse.register_device(Railway::Latch.new("latch", *latch_inputs))
+
+
+fse.register_device(Railway::Train.new("train1", 15, ["r1", "r2", "r4"], facilities))
+fse.register_device(Railway::Train.new("train2", 15, ["r5", "r6", "r8"], facilities))
+
+#pp facilities
+
+fse.start
+
