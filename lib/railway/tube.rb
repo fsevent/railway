87d5b9a5d22d1127be715f08abfae807fc2d6b95
Tanaka Akira <akr@fsij.org>
2015-12-16 17:46:58 +0900

    Railway::Tube implemented.

---
+++ lib/railway/tube.rb
@@ -0,0 +1,117 @@
+# tube.rb --- cylindrical space
+
+require 'matrix'
+
+class Railway::Tube
+  # pos1 : Vector of three numbers
+  # pos2 : Vector of three numbers
+  # radius : positive number
+  def initialize(pos1, pos2, radius)
+    @pos1 = pos1.map(&:to_f)
+    @pos2 = pos2.map(&:to_f)
+    @radius = radius.to_f
+  end
+  attr_reader :pos1, :pos2, :radius
+
+  def bounding_box
+    [@pos1.map2(@pos2) {|*pair| pair.min - @radius },
+     @pos1.map2(@pos2) {|*pair| pair.max + @radius }]
+  end
+
+  def interfere?(other)
+    ap1, ap2, ar = @pos1, @pos2, @radius
+    bp1, bp2, br = other.pos1, other.pos2, other.radius
+
+    # doesn't intefere if two bounding box is not interfere.
+    amin, amax = self.bounding_box
+    bmin, bmax = other.bounding_box
+    amax.each2(bmin) {|max, min| return false if max < min }
+    bmax.each2(amin) {|max, min| return false if max < min }
+
+    ar_br = ar + br
+
+    # intefere if tip sphere interfere.
+    [ap1, ap2].each {|ap|
+      [bp1, bp2].each {|bp|
+        return true if (ap - bp).r <= ar_br
+      }
+    }
+
+    av = ap2 - ap1
+    bv = bp2 - bp1
+
+    n = av.cross(bv)
+    nl = n.r
+    if nl == 0.0 # xxx: dangerous to test equality of float numbers.
+      # parallel
+      ap1_bp1 = (ap1 - bp1)
+      bn = bv.normalize
+      hv = ap1_bp1.cross(bn)
+      h = hv.r
+      # doesn't intefere if parallel lines are far enough.
+      return false if ar_br < h
+      d1 = bn.dot(ap1_bp1)
+      d2 = bn.dot(ap2 - bp1)
+      # doesn't intefere if parallel line segments are slid. (condition 1)
+      return false if d1 < 0 && d2 < 0
+      bl = bv.r
+      # doesn't intefere if parallel line segments are slid. (condition 2)
+      return false if bl < d1 && bl < d2
+      # interfere otherwise.
+      return true
+    end
+
+    # not parallel
+
+    # doesn't intefere if the nearest points are far enough.
+    n /= nl
+    h = n.dot(ap1-bp1).abs
+    return false if ar_br < h
+
+    al = av.r
+    bl = bv.r
+
+    an = av.normalize
+    bn = bv.normalize
+
+    anbn = an.dot(bn)
+
+    den = 1 - anbn ** 2
+
+    ap1_bp1 = (ap1 - bp1)
+    da = (ap1_bp1.dot(bn) * anbn - ap1_bp1.dot(an)) / den
+    db = (- ap1_bp1.dot(an) * anbn + ap1_bp1.dot(bn)) / den
+
+    # the nearest point in ap1 to ap2.  nil if it is outside of ap1 to ap2.
+    ap0 = nil
+    if 0 <= da && da <= al
+      ap0 = ap1 + an * da
+    end
+
+    # the nearest point in bp1 to bp2.  nil if it is outside of bp1 to bp2.
+    bp0 = nil
+    if 0 <= db && db <= bl
+      bp0 = bp1 + bn * db
+    end
+
+    if ap0 && bp0
+      # interfere.  the nearest points are near enough because h <= ar_br.
+      return true
+    end
+    if bp0
+      # interfere if the nearest point is interfer tip spheres on the other line.
+      [ap1, ap2].each {|ap|
+        return true if (ap - bp0).r <= ar_br
+      }
+    end
+    if ap0
+      # interfere if the nearest point is interfer tip spheres on the other line.
+      [bp1, bp2].each {|bp|
+        return true if (ap0 - bp).r <= ar_br
+      }
+    end
+
+    # doesn't interfere otherwise.
+    return false
+  end
+end
---
+++ test/test_tube.rb
@@ -0,0 +1,76 @@
+require 'test/unit'
+require 'pp'
+
+module Railway
+end
+
+require 'railway/tube'
+
+class TestRailwayTube < Test::Unit::TestCase
+  def assert_interfere(tube1, tube2, message=nil)
+    assert_true(tube1.interfere?(tube2), message)
+    assert_true(tube2.interfere?(tube1), message)
+  end
+
+  def assert_not_interfere(tube1, tube2, message=nil)
+    assert_false(tube1.interfere?(tube2), message)
+    assert_false(tube2.interfere?(tube1), message)
+  end
+
+  def test_interfere_bounding_box
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,0,0], 1)
+    tube2 = Railway::Tube.new(Vector[20,0,0], Vector[30,0,0], 1)
+    assert_not_interfere(tube1, tube2)
+  end
+
+  def test_interfere_connected
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,0,0], 1)
+    tube2 = Railway::Tube.new(Vector[10,0,0], Vector[30,0,0], 1)
+    assert_interfere(tube1, tube2)
+  end
+
+  def test_interfere_parallel_far
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,10,0], 1)
+    tube2 = Railway::Tube.new(Vector[5,8,0], Vector[15,18,0], 1)
+    assert_not_interfere(tube1, tube2)
+  end
+
+  def test_interfere_parallel_near
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,10,0], 2)
+    tube2 = Railway::Tube.new(Vector[5,8,0], Vector[15,18,0], 2)
+    assert_interfere(tube1, tube2)
+  end
+
+  def test_interfere_collinear
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,10,0], 5)
+    tube2 = Railway::Tube.new(Vector[18,18,0], Vector[30,30,0], 5)
+    assert_not_interfere(tube1, tube2)
+  end
+
+  def test_nearest_point_far
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,10,0], 1)
+    tube2 = Railway::Tube.new(Vector[2,8,-10], Vector[2,8,10], 1)
+    assert_not_interfere(tube1, tube2)
+  end
+
+  def test_nearest_point_near
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,10,0], 1)
+    tube2 = Railway::Tube.new(Vector[5,5,-10], Vector[5,5,10], 1)
+    assert_interfere(tube1, tube2)
+  end
+
+  def test_nearest_point_near_but_out_of_bound_half
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,10,0], 1)
+    tube2 = Railway::Tube.new(Vector[5,5,1], Vector[5,5,10], 1)
+    assert_interfere(tube1, tube2)
+  end
+
+  def test_nearest_point_far_out_of_bound_both
+    tube1 = Railway::Tube.new(Vector[0,0,0], Vector[10,10,0], 2)
+    tube2 = Railway::Tube.new(Vector[13,13,2], Vector[13,13,10], 2)
+    assert_not_interfere(tube1, tube2)
+  end
+
+
+end
+
